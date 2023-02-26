13b4916f464e5807d8929efed506fac0d8cfa4dd
Chhatrapati Chauhan <116446768+chhatrapati295@users.noreply.github.com>
2023-02-26 09:07:47 +0530

    More Efficient js logic

--- file.js
+++ file.js
@@ -1,26 +1,30 @@
-const input = document.getElementById('input')
-const btn = document.getElementById('btn')
-const box1 = document.getElementById('box1')
-input.addEventListener('keyup',(e)=>{
-    if(e.key==='Enter' ){
-        const newli = document.createElement('li')
-    newli.innerHTML = `<h2 class='liItem'></h2>`
-    newli.textContent += input.value
-        newli.innerHTML += `<button class='removeBtn' id='btn'><i class="fa-solid fa-trash-can"></i></button>`
-        newli.querySelector('.removeBtn').addEventListener('click',()=>{
-            newli.remove()
-        })
-    input.value= ''
-    box1.after(newli)
+const form = document.querySelector('form')
+const input = document.querySelector('.input')
+const box = document.querySelector('.box')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    if(input.value){
+        addData()
+    }else{
+        input.value = 'First write something'
+        setTimeout(() => {
+            input.value = ''
+        }, 2000);
     }
-}) || btn.addEventListener('click',()=>{
-    const newli = document.createElement('li')
-    newli.innerHTML = `<h2 class='liItem'></h2>`
-    newli.textContent += input.value
-        newli.innerHTML += `<button class='removeBtn' id='btn'><i class="fa-solid fa-trash-can"></i></button>`
-        newli.querySelector('.removeBtn').addEventListener('click',()=>{
-            newli.remove()
-        })
-    input.value= ''
-    box1.after(newli)
 })
+
+const addData = ()=>{
+    const div = document.createElement('p')
+    div.setAttribute('class','list')
+    div.innerHTML = `
+    <p>${input.value}</p>
+    <button class='btnRemove'>Remove</button>
+    `
+    input.value = ''
+    box.append(div)
+    Array.from(document.querySelectorAll('.btnRemove')).forEach(button=>{
+        button.addEventListener('click',(e)=>{
+        e.target.parentNode.remove()
+        })
+    })
+}
